e478c2f46f680d318db4e7e49cba608cd8521d5e
Riodotche <daninidotche@gmail.com>
2022-01-20 17:35:44 +0100

    add -init files

---
+++ FizzBuzz.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class FizzBuzz
+{
+    // tous les multiple de 3 sont remplacé par Fizz
+    // tous les multiples de 5 sont remplacé par Buzz
+    // tous les multiples de 3 et 5 sont remplacé par FizzBuzz
+}
